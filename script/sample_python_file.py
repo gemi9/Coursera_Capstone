c3d0ffea11f4865782062230d64db2374b603c9e
gemi9 <rakeshreddy.kondam@gmail.com>
2020-08-06 18:57:18 +0530

    Updated line at the end

--- script/sample_python_file.py
+++ script/sample_python_file.py
@@ -1,6 +1,5 @@
-## This is the first python script for the Coursera Castone project IBM
-print('Hello World!')
+# Importing the libraties
 import pandas as import pd
 import numpy as np
-## Test Run
-print('Second Statement remains same - Capstone')
+## Test line
+print('Hi Coursera Capstone Project!')
