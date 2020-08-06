baceaf8c5d5f30413975bafef960e53495464c87
gemi9 <rakeshreddy.kondam@gmail.com>
2020-08-06 16:02:13 +0530

    Created a Capstone Folder and added a Python Script

---
+++ script/sample_python_file.py
@@ -0,0 +1,6 @@
+## This is the first python script for the Coursera Castone project IBM
+print('Hello World!')
+import pandas as import pd
+import numpy as np
+## Test Run
+print('Second Statement remains same - Capstone')
